f21791d22605a67044cd83b1c62b31ccb46a001f
edmundloo <edmundloo92@gmail.com>
2021-04-27 17:04:30 +0800

    edited item_form.html
    added checkbox quantity validation
    added checkbox reset on form submit
    added product table (incomplete)

--- js/formScript.js
+++ js/formScript.js
@@ -1,3 +1,44 @@
+//For checkbox and quantity input validation
+document.querySelector("#checkBlack").onchange = function () {
+    document.querySelector("#qtyBlack").disabled = !this.checked;
+    document.querySelector("#qtyBlack").value = "";
+    document.querySelector("#qtyBlack").required = this.checked;
+};
+
+document.querySelector("#checkBlue").onchange = function () {
+    document.querySelector("#qtyBlue").disabled = !this.checked;
+    document.querySelector("#qtyBlue").value = "";
+    document.querySelector("#qtyBlue").required = this.checked;
+};
+
+document.querySelector("#checkBrown").onchange = function () {
+    document.querySelector("#qtyBrown").disabled = !this.checked;
+    document.querySelector("#qtyBrown").value = "";
+    document.querySelector("#qtyBrown").required = this.checked;
+};
+
+document.querySelector("#checkPink").onchange = function () {
+    document.querySelector("#qtyPink").disabled = !this.checked;
+    document.querySelector("#qtyPink").value = "";
+    document.querySelector("#qtyPink").required = this.checked;
+};
+
+document.querySelector("#checkRed").onchange = function () {
+    document.querySelector("#qtyRed").disabled = !this.checked;
+    document.querySelector("#qtyRed").value = "";
+    document.querySelector("#qtyRed").required = this.checked;
+};
+
+document.querySelector("#checkWhite").onchange = function () {
+    document.querySelector("#qtyWhite").disabled = !this.checked;
+    document.querySelector("#qtyWhite").value = "";
+    document.querySelector("#qtyWhite").required = this.checked;
+};
+
+
+
+
+//For saving of user input form data
 var formList = [];
 
 function resetError() {
@@ -5,6 +46,15 @@ function resetError() {
     document.querySelector("#itemCodeErrorMsg").innerHTML = "";
     document.querySelector("#itemNameErrorMsg").innerHTML = "";
     document.querySelector("#itemDescriptionErrorMsg").innerHTML = "";
+}
+
+function resetForm() {
+    document.querySelector("#item_form").reset();
+
+    var qtyInput = document.querySelectorAll('.qty-input');
+    for (let i = 0; i < qtyInput.length; i++) {
+        qtyInput[i].setAttribute("disabled", "disabled");
+    };
 }
 
 function submitForm() {
@@ -15,7 +65,7 @@ function submitForm() {
     var itemName = document.querySelector("#item-name").value;
     var itemPrice = document.querySelector("#item-price").value;
     var itemDescript = document.querySelector("#item-description").value;
-    var itemImage = "";                              //image??
+    var itemImage = "";                                                                 //image??
 
     var colorArray = [];
     if (document.querySelector("#checkBlack").checked == true) {
@@ -89,7 +139,7 @@ function submitForm() {
 
     if (foundColor && foundCode && foundName && foundDescript) {
         addToList(itemCode, itemName, colorArray, itemPrice, itemDescript, itemImage);
-        document.querySelector("#item_form").reset();
+        resetForm();
     }
 }
 
@@ -109,4 +159,70 @@ function addToList(itemCode, itemName, colorArray, itemPrice, itemDescript, item
 function listForm() {
     console.log(`Total items: ${formList.length}`);
     console.log(formList);
+    appendData(formList);
+}
+
+
+
+
+
+
+//For saving of user input data into a table
+let data = [{
+    code: 31263,
+    name: "Men's Backpack",
+    colorArray: ["Black", "Blue", "Red", "White"],
+    price: 69.90,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    image: "images/bag1.jpg"
+},
+{
+    code: 12312,
+    name: "Ladies' Backpack",
+    colorArray: ["Black", "Pink", "White"],
+    price: 59.90,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    image: "images/bag2.jpg"
+},
+{
+    code: 54567,
+    name: "Children's Backpack",
+    colorArray: ["Black", "Pink"],
+    price: 25.90,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    image: "images/bag3.jpg"
+}];
+
+function createTable(dataItem) {
+    `
+    <tr>
+    <td>${dataItem.iCode}</td>
+    <td>${dataItem.iName}</td>
+    <td>${dataItem.iColorArray}</td>
+    <td>${dataItem.iPrice}</td>
+    <td>${dataItem.iDescript}</td>
+    <td><img src="${dataItem.iItemImage}"></td>
+    </tr>
+`
+}
+
+function appendData(userData) {
+
+    let myTableHeader = "<table id='userTable'><tr><th>Item Code</th><th>Item Name</th><th>Colors Available</th><th>Item Price</th><th>Item Description</th><th>Item Image</th></tr>";
+    let myTableEnd = "</table>";
+
+    let dataList = [];
+
+    dataList.push(myTableHeader);
+    console.log(dataList);
+
+    for (let i = 0; i < userData.length; i++) {
+        let myTableContent = createTable(userData[i]);
+        dataList.push(myTableContent);
+    }
+
+    dataList.push(myTableEnd);
+
+    const pHTML = dataList.join("\n");
+    document.querySelector("#dataTable").innerHTML = pHTML;
 }
